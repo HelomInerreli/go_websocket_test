a14656e39b35654acb6f4149d60c8c5b9b561e51
Helom Valentim <helom.valentim.t0127856@edu.atec.pt>
2026-08-15 20:16:58 +0100

    Embed static files directly into Go binary for Render deployment

--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
 )
 
+//go:embed static/*
+var staticFiles embed.FS
+
 func main() {
 	hub := newHub()
 	go hub.run()
 
-	// Servidor de ficheiros estáticos para a interface HTML/CSS/JS
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
+	// Extrai a subpasta static dos ficheiros embebidos no próprio binário
+	subFS, err := fs.Sub(staticFiles, "static")
+	if err != nil {
+		log.Fatalf("Erro ao carregar ficheiros estáticos embebidos: %v", err)
+	}
+
+	// Servidor de ficheiros estáticos (agora embutidos 100% no binário Go)
+	http.Handle("/", http.FileServer(http.FS(subFS)))
 
 	// Endpoint WebSocket
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
@@ -28,7 +38,7 @@ func main() {
 	}
 
 	log.Printf("Servidor de Chat WebSocket a rodar na porta %s", port)
-	err := http.ListenAndServe(port, nil)
+	err = http.ListenAndServe(port, nil)
 	if err != nil {
 		log.Fatalf("Erro ao iniciar servidor: %v", err)
 	}
